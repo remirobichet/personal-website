367ce41f0a24e83d68a56b3e84fb86f5a4117933
Rémi Robichet <remi.robichet@cgi.com>
2020-03-02 12:29:48 +0100

    :sparkles: Add dark theme

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,30 @@ import {httpGetData} from './http'
 import {ready} from './base'
 
 const lang = window.localStorage.getItem('lang') || 'fr';
+const theme = window.localStorage.getItem('theme') || 'light';
 
 /* Set lang on html tag */
 document.documentElement.lang = lang;
+document.documentElement.dataset.theme = theme
 
 httpGetData(document.documentElement.lang);
 
 ready(() => {
+    /* Set lang */
     document.querySelector(`.lang-switcher[data-lang="${lang}"]`).classList.add('active');
     document.querySelector('.lang-switcher:not(.active)').addEventListener('click', () => {
         localStorage.setItem('lang', document.querySelector('.lang-switcher:not(.active)').dataset.lang);
         window.location.reload();
+    });
+
+    /* Set theme */
+    document.querySelector(`.theme-switcher[data-theme="${theme}"]`).classList.add('active');
+    document.querySelectorAll('.theme-switcher').forEach((value) => {
+        value.addEventListener('click', function() {
+            localStorage.setItem('theme', this.dataset.theme);
+            document.documentElement.dataset.theme = this.dataset.theme;
+            [...this.parentNode.children].forEach(el => el.classList.remove('active'));
+            this.classList.add('active');
+        });
     });
 });
