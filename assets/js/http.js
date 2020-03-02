8e79c4aee99e5bf6651e6a17de0e2f619199f4e0
Rémi Robichet <remi.robichet@cgi.com>
2020-03-02 15:31:21 +0100

    :sparkles: Add multilanguage done

--- assets/js/http.js
+++ assets/js/http.js
@@ -1,4 +1,5 @@
 import Mustache from 'mustache';
+import {portfolio, intro, previous, sections, title} from '/assets/i18n/i18n.json';
 
 let httpGetFromType = (type, lang = '') => {
     let request = new XMLHttpRequest();
@@ -15,10 +16,32 @@ let httpGetFromType = (type, lang = '') => {
     request.send();
 };
 
+let getStaticData = (type, lang = '') => {
+    let template = document.querySelector(`[data-template="${type.name}"]`);
+    template.innerHTML = Mustache.render(template.innerHTML, {data: type[lang]});
+    template.classList.add('loaded');
+};
+
+let getSectionNameData = (type, lang = '') => {
+    let templates = document.querySelectorAll(`[data-text]`);
+    templates.forEach((template) => {
+        template.dataset.text = type[lang][template.dataset.text]
+    });
+};
+
+let getTitleData = (type, lang = '') => {
+    document.title = type[lang];
+};
+
 let httpGetData = (lang) => {
     httpGetFromType('skills');
     httpGetFromType('career', lang);
     httpGetFromType('contact');
+    getStaticData(portfolio, lang);
+    getStaticData(intro, lang);
+    getStaticData(previous, lang);
+    getSectionNameData(sections, lang);
+    getTitleData(title, lang);
 };
 
 export { httpGetData };
