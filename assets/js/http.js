0a2b0266a2fb51765b52562d51631b5a988e050d
Rémi Robichet <remi.robichet@cgi.com>
2020-03-13 14:52:02 +0100

    :boom: Refacto new API

--- assets/js/http.js
+++ assets/js/http.js
@@ -1,53 +1,48 @@
 import {ready} from './base'
 import Mustache from 'mustache';
-import {portfolio, intro, previous, sections, cv, title} from '/assets/i18n/i18n.json';
 
-let httpGetFromType = (type, lang = '') => {
+let httpGetFromType = (type, lang, callback) => {
     let request = new XMLHttpRequest();
-    const url = `https://functions.remirobichet.fr/.netlify/functions/resume?type=${type}.json${lang ? `&lang=${lang}` : ''}`;
+    const url = `https://functions.remirobichet.fr/.netlify/functions/api/${type}?lang=${lang}`;
     request.open('GET', url, true);
     request.onload = () => {
         if (request.status >= 200 && request.status < 400) {
-            const data = JSON.parse(request.response);
-            let template = document.querySelector(`[data-template="${type}"]`);
-            template.innerHTML = Mustache.render(template.innerHTML, {data});
-            template.classList.add('loaded');
+            let data = {...data, ...JSON.parse(request.response)};
+            callback(data);
         }
     };
     request.send();
 };
 
-let getStaticData = (type, lang = '') => {
-    let template = document.querySelector(`[data-template="${type.name}"]`);
-    console.log(template.innerHTML)
-    template.innerHTML = Mustache.render(template.innerHTML, {data: type[lang]});
-    template.classList.add('loaded');
-};
-
-let getSectionNameData = (type, lang = '') => {
-    let templates = document.querySelectorAll(`[data-text]`);
-    templates.forEach((template) => {
-        template.dataset.text = type[lang][template.dataset.text];
-        template.classList.add('loaded');
+let setData = (data) => {
+    ready(() => {
+        Object.entries(data).forEach(([key, el]) => {
+            let template = document.querySelector(`[data-template="${key}"]`);
+            if (template) {
+                if (typeof el === 'string') {
+                    template.innerHTML = el;
+                } else {
+                    template.innerHTML = Mustache.render(template.innerHTML, {data: el});
+                    template.classList.add('loaded');
+                }
+            }
+            if (key === 'title') {
+                document.title = el;
+            }
+            if (key === 'sections') {
+                let text = document.querySelectorAll(`[data-text]`);
+                text.forEach((template) => {
+                    template.dataset.text = el[template.dataset.text];
+                    template.classList.add('loaded');
+                });
+            }
+        });
     });
 };
 
-let getTitleData = (type, lang = '') => {
-    document.title = type[lang];
-};
-
 let httpGetData = (lang) => {
-    httpGetFromType('skills');
-    httpGetFromType('career', lang);
-    httpGetFromType('contact');
-    ready(() => {
-        getStaticData(cv, lang);
-        getStaticData(portfolio, lang);
-        getStaticData(intro, lang);
-        getStaticData(previous, lang);
-        getSectionNameData(sections, lang);
-        getTitleData(title, lang);
-    });    
+    httpGetFromType('me', lang, setData);
+    httpGetFromType('website', lang, setData);
 };
 
-export { httpGetData };
+export {httpGetData};
