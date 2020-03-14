1317cb652188e34885aa672727434a3af300bff0
Rémi Robichet <remi.robichet@cgi.com>
2020-03-14 11:22:03 +0100

    :art: Improve code structure

--- assets/js/http.js
+++ assets/js/http.js
@@ -1,9 +1,11 @@
 import {ready} from './base'
 import Mustache from 'mustache';
 
-let httpGetFromType = (type, lang, callback) => {
+const API_URL = 'https://functions.remirobichet.fr/.netlify/functions/api/';
+
+let httpGetFromApi = (api, lang, callback) => {
     let request = new XMLHttpRequest();
-    const url = `https://functions.remirobichet.fr/.netlify/functions/api/${type}?lang=${lang}`;
+    const url = `${API_URL}${api}?lang=${lang}`;
     request.open('GET', url, true);
     request.onload = () => {
         if (request.status >= 200 && request.status < 400) {
@@ -41,8 +43,8 @@ let setData = (data) => {
 };
 
 let httpGetData = (lang) => {
-    httpGetFromType('me', lang, setData);
-    httpGetFromType('website', lang, setData);
+    httpGetFromApi('me', lang, setData);
+    httpGetFromApi('website', lang, setData);
 };
 
 export {httpGetData};
