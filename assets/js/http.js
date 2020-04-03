7193d0575ac0865c13a7f47061116d52e8ba3d61
Rémi Robichet <remi.robichet@gmail.com>
2020-04-03 13:40:47 +0200

    :bug: Fix parsing timing

--- assets/js/http.js
+++ assets/js/http.js
@@ -4,16 +4,18 @@ import Mustache from 'mustache';
 const API_URL = 'https://functions.remirobichet.fr/.netlify/functions/api/';
 
 let httpGetFromApi = (api, lang, callback) => {
-    let request = new XMLHttpRequest();
-    const url = `${API_URL}${api}?lang=${lang}`;
-    request.open('GET', url, true);
-    request.onload = () => {
-        if (request.status >= 200 && request.status < 400) {
-            let data = {...data, ...JSON.parse(request.response)};
-            return callback(data);
-        }
-    };
-    request.send();
+    return new Promise((resolve) => {
+        let request = new XMLHttpRequest();
+        const url = `${API_URL}${api}?lang=${lang}`;
+        request.open('GET', url, true);
+        request.onload = () => {
+            if (request.status >= 200 && request.status < 400) {
+                let data = {...data, ...JSON.parse(request.response)};
+                return callback(data).then(() => resolve());
+            }
+        };
+        request.send();
+    });
 };
 
 let setData = (data) => {
