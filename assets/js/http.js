3d7a77cc172e58fdc42b25aa1fd662fa2868a01b
Rémi Robichet <remi.robichet@gmail.com>
2020-04-03 10:50:10 +0200

    :sparkles: Implement tag-to-emoji

--- assets/js/http.js
+++ assets/js/http.js
@@ -10,41 +10,50 @@ let httpGetFromApi = (api, lang, callback) => {
     request.onload = () => {
         if (request.status >= 200 && request.status < 400) {
             let data = {...data, ...JSON.parse(request.response)};
-            callback(data);
+            return callback(data);
         }
     };
     request.send();
 };
 
 let setData = (data) => {
-    ready(() => {
-        Object.entries(data).forEach(([key, el]) => {
-            let template = document.querySelector(`[data-template="${key}"]`);
-            if (template) {
-                if (typeof el === 'string') {
-                    template.innerHTML = el;
-                } else {
-                    template.innerHTML = Mustache.render(template.innerHTML, {data: el});
-                    template.classList.add('loaded');
+    return new Promise((resolve) => {
+        ready(() => {
+            Object.entries(data).forEach(([key, el]) => {
+                let template = document.querySelector(`[data-template="${key}"]`);
+                if (template) {
+                    if (typeof el === 'string') {
+                        template.innerHTML = el;
+                    } else {
+                        template.innerHTML = Mustache.render(template.innerHTML, {data: el});
+                        template.classList.add('loaded');
+                    }
                 }
-            }
-            if (key === 'title') {
-                document.title = el;
-            }
-            if (key === 'sections') {
-                let text = document.querySelectorAll(`[data-text]`);
-                text.forEach((template) => {
-                    template.dataset.text = el[template.dataset.text];
-                    template.classList.add('loaded');
-                });
-            }
+                if (key === 'title') {
+                    document.title = el;
+                }
+                if (key === 'sections') {
+                    let text = document.querySelectorAll(`[data-text]`);
+                    text.forEach((template) => {
+                        template.dataset.text = el[template.dataset.text];
+                        template.classList.add('loaded');
+                    });
+                }
+            });
+            resolve();
         });
     });
+
 };
 
 let httpGetData = (lang) => {
-    httpGetFromApi('me', lang, setData);
-    httpGetFromApi('website', lang, setData);
+    return new Promise((resolve) => {
+        const me = httpGetFromApi('me', lang, setData);
+        const website = httpGetFromApi('website', lang, setData);
+        Promise.all([me, website]).then(() =>{
+            resolve();
+        });
+    });
 };
 
 export {httpGetData};
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,40 @@
-import {httpGetData} from './http'
-import {ready} from './base'
+import {httpGetData} from './http';
+import {ready} from './base';
+import {parse} from 'tag-to-emoji';
 
 const lang = window.localStorage.getItem('lang') || 'fr';
 const theme = window.localStorage.getItem('theme') || 'light';
 
 /* Set lang on html tag */
 document.documentElement.lang = lang;
-document.documentElement.dataset.theme = theme
+document.documentElement.dataset.theme = theme;
 
-httpGetData(document.documentElement.lang);
+httpGetData(document.documentElement.lang).then(() => onReady());
 
-ready(() => {
-    /* Set lang */
-    document.querySelector(`.lang-switcher[data-lang="${lang}"]`).classList.add('active');
-    document.querySelector('.lang-switcher:not(.active)').addEventListener('click', () => {
-        localStorage.setItem('lang', document.querySelector('.lang-switcher:not(.active)').dataset.lang);
-        window.location.reload();
-    });
+let onReady = () => {
+    ready(() => {
+        /* Parse tag-to-emoji */
+        let body = document.querySelector('body');
+        body.innerHTML = parse(body.innerHTML);
 
-    /* Set theme */
-    document.querySelector(`.theme-switcher[data-theme="${theme}"]`).classList.add('active');
-    document.querySelectorAll('.theme-switcher').forEach((value) => {
-        value.addEventListener('click', function() {
-            localStorage.setItem('theme', this.dataset.theme);
-            document.documentElement.dataset.theme = this.dataset.theme;
-            [...this.parentNode.children].forEach(el => el.classList.remove('active'));
-            this.classList.add('active');
+        /* Set lang */
+        console.log(document.querySelector(`.lang-switcher[data-lang="${lang}"]`));
+        document.querySelector(`.lang-switcher[data-lang="${lang}"]`).classList.add('active');
+        document.querySelector('.lang-switcher:not(.active)').addEventListener('click', () => {
+            localStorage.setItem('lang', document.querySelector('.lang-switcher:not(.active)').dataset.lang);
+            window.location.reload();
+        });
+
+        /* Set theme */
+        document.querySelector(`.theme-switcher[data-theme="${theme}"]`).classList.add('active');
+        document.querySelectorAll('.theme-switcher').forEach((value) => {
+            value.addEventListener('click', function() {
+                localStorage.setItem('theme', this.dataset.theme);
+                document.documentElement.dataset.theme = this.dataset.theme;
+                [...this.parentNode.children].forEach(el => el.classList.remove('active'));
+                this.classList.add('active');
+            });
         });
     });
-});
+};
+
