dc31ca09f927b63b7c6a1a61a3b8400eafc17f97
Rémi Robichet <remi.robichet@gmail.com>
2020-04-03 13:28:08 +0200

    :bug: Fix parsing timing

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,14 +9,16 @@ const theme = window.localStorage.getItem('theme') || 'light';
 document.documentElement.lang = lang;
 document.documentElement.dataset.theme = theme;
 
-httpGetData(document.documentElement.lang).then(() => onReady());
+httpGetData(document.documentElement.lang).then(() => {
+    /* Parse tag-to-emoji */
+    let body = document.querySelector('body');
+    body.innerHTML = parse(body.innerHTML);
+
+    onReady()
+});
 
 let onReady = () => {
     ready(() => {
-        /* Parse tag-to-emoji */
-        let body = document.querySelector('body');
-        body.innerHTML = parse(body.innerHTML);
-
         /* Set lang */
         document.querySelector(`.lang-switcher[data-lang="${lang}"]`).classList.add('active');
         document.querySelector('.lang-switcher:not(.active)').addEventListener('click', () => {
